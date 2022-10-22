67bdec22e92fdef77f2717678436d43595d0b710
BinaryShock7 <18030767@itcelaya.edu.mx>
2022-10-22 18:48:50 -0500

    Primer Commit

---
+++ lib/screens/detail_character_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailCharacterScreen extends StatefulWidget {
+  const DetailCharacterScreen({super.key});
+
+  @override
+  State<DetailCharacterScreen> createState() => _DetailCharacterScreenState();
+}
+
+class _DetailCharacterScreenState extends State<DetailCharacterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/list_character_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListCharacterScreen extends StatefulWidget {
+  const ListCharacterScreen({super.key});
+
+  @override
+  State<ListCharacterScreen> createState() => _ListCharacterScreenState();
+}
+
+class _ListCharacterScreenState extends State<ListCharacterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
